ce6dacf06b81dbdefb8a8e1ebacda3251de8c039
rchan8510-source <rchan8510@gmail.com>
2025-09-25 13:56:54 +0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request) {
-    const url = new URL(request.url);
-    const to = url.searchParams.get("to");
-    if (!to) return new Response("Missing ?to=", { status: 400 });
-    return Response.redirect(to, 302);
-  }
-}
